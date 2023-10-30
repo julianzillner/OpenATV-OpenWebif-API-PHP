1dc52bc5c7f8249db834e948c1f3c1caeef2fdda
julianzillner <info@julianzillner.com>
2023-10-31 00:04:43 +0100

    made a clean code

--- index.php
+++ index.php
@@ -13,21 +13,21 @@ $data = json_decode($json, true);
     <title>OpenWebif Status</title>
   </head>
   <body>
-  <div class="p-3 mb-2 bg-black text-white"><b>OpenWebif Status by Julian Zillner</b></div>
-  <div class="container main-container"><br>
-    <h2>Current Station: <span class="badge bg-secondary"><?php echo htmlentities($data['currservice_station']); ?></span></h2>
-<h4><?php echo htmlentities($data['currservice_name']); ?></h4>
-<p><?php echo htmlentities($data['currservice_fulldescription']); ?><br></p>
-<p style="color:red"><b><?php echo htmlentities($data['currservice_begin']); ?>-<?php echo htmlentities($data['currservice_end']); ?></b></p>
-</div>
-<div class="p-3 mb-2 bg-black text-white"></div>
-<div class="container main-container">
-<h2>Another Details:</h2>
-<p><b>Standby?</b> <?php echo htmlentities($data['inStandby']); ?>
-    <br><b>Current Volume:</b> <?php echo htmlentities($data['volume']); ?>%<br>
-    <b>Live recording:</b> <?php echo htmlentities($data['isRecording']); ?></p>
-</div>
-<div class="fixed-bottom">
-<div class="p-3 mb-2 bg-black text-white"><center>Code by Julian Zillner</center></div>
+    <div class="p-3 mb-2 bg-black text-white"><b>OpenWebif Status by Julian Zillner</b></div>
+      <div class="container main-container"><br>
+          <h2>Current Station: <span class="badge bg-secondary"><?php echo htmlentities($data['currservice_station']); ?></span></h2>
+          <h4><?php echo htmlentities($data['currservice_name']); ?></h4>
+          <p><?php echo htmlentities($data['currservice_fulldescription']); ?><br></p>
+          <p style="color:red"><b><?php echo htmlentities($data['currservice_begin']); ?>-<?php echo htmlentities($data['currservice_end']); ?></b></p>
+        </div>
+      <div class="p-3 mb-2 bg-black text-white"></div>
+          <div class="container main-container">
+              <h2>Another Details:</h2>
+              <p><b>Standby?</b> <?php echo htmlentities($data['inStandby']); ?>
+              <br><b>Current Volume:</b> <?php echo htmlentities($data['volume']); ?>%<br>
+              <b>Live recording:</b> <?php echo htmlentities($data['isRecording']); ?></p>
+          </div>
+        <div class="fixed-bottom">
+            <div class="p-3 mb-2 bg-black text-white"><center>Code by Julian Zillner</center></div>
   </body>
 </html>
